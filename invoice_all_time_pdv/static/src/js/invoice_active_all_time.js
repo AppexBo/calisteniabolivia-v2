caa3116227b1a9e2d7cf8fb443c13540e90719fc
Jhenyfer <leonardo.eguez@appexbo.com>
2025-05-01 10:34:42 -0400

    dev invoice_all_time_pdv erick

---
+++ invoice_all_time_pdv/static/src/js/invoice_active_all_time.js
@@ -0,0 +1,40 @@
+/** @odoo-module */
+
+import { Order } from "@point_of_sale/app/store/models";
+import { patch } from "@web/core/utils/patch";
+
+patch(Order.prototype, {
+    setup() {
+        super.setup(...arguments);
+        this.changePos(this.pos);
+	}, 
+
+    changePos(pos){
+        // Crear un MutationObserver para observar cambios en el DOM
+        const observer = new MutationObserver(() => {            
+            this.active_invoice_all_time();
+        });
+        
+        // Observar cambios en el DOM dentro del contenedor principal
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    },
+
+    active_invoice_all_time(){
+        const invoice_button = document.querySelector('.js_invoice');
+        if(invoice_button){
+            // Verifica si el botón existe y no tiene las clases "highlight" o "text-bg-primary"
+            if (invoice_button && !invoice_button.classList.contains('highlight') && !invoice_button.classList.contains('text-bg-primary')) {
+                //console.log("simulate click");
+                invoice_button.click();
+            }
+            //ocultar el boton siempre
+            if(invoice_button && invoice_button.style.display === ''){
+                //console.log("simulate hide");
+                invoice_button.setAttribute('style', 'display: none !important;');
+            }
+        }
+    }
+});
