e2951848ad4dacfcbfdf31c53aab9269001606d3
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-05-03 10:39:19 -0400

    Probando Reporte fix

--- reporte_orden_trabajo/models/project_task.py
+++ reporte_orden_trabajo/models/project_task.py
@@ -14,15 +14,17 @@ class SecuenciaMes(models.Model):
         compute='_compute_mes_secuencia',
         store=True
     )
+
     def number_to_word(self, number: float):
         decimal_part = int(round(number % 1, 2) * 100)
         integer_part = int(number)
-        # get actual language
+        # obtener el idioma actual
         lang = self.env.context.get('lang', 'es_ES')
         return f"{num2words(integer_part, lang=lang)} con {decimal_part}/100 {self.currency_id.symbol}"
+
     def amount_to_text(self, amount):
         # Usar num2words para convertir el monto en número a texto
-        return num2words.num2words(amount, lang='es')
+        return num2words(amount, lang='es')
     @api.depends('date_order')
     def _compute_mes_secuencia(self):
         for record in self:
