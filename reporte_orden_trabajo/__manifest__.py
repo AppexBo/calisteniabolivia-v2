bfd650be27499ea91d020253adc638bbbe390316
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-05-03 19:30:00 -0400

    Terminado, en revision

--- reporte_orden_trabajo/__manifest__.py
+++ reporte_orden_trabajo/__manifest__.py
@@ -11,11 +11,12 @@
     'author': 'APPEX BOLIVIA SRL.',
     'summary': 'Reporte Orden Trabajo v18',
     'data': [
+        'views/project_task_add_fields.xml',
     'reports/formato_papel.xml',
       'reports/boton_imprimir_pdf.xml',
       'reports/cabecera_pdf.xml',
       'reports/cuerpo_pdf.xml',
-      'views/project_task_add_fields.xml',
+      
     ],
     'installable': True,
 }
