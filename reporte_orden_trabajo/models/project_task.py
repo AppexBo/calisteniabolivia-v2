d7d938b4ede70dce10356ab8176dca7b039dd6ae
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-05-03 16:02:46 -0400

    Cambio

--- reporte_orden_trabajo/models/project_task.py
+++ reporte_orden_trabajo/models/project_task.py
@@ -4,7 +4,29 @@ from dateutil.relativedelta import relativedelta
 from num2words import num2words
 from datetime import datetime
 
-                
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+    _description = "Project Task"
+
+    tipo_vehiculo = fields.Char(string='Tipo Vehículo', store=True, required=True)
+    color_vehiculo = fields.Char(string='Color', store=True, required=True)
+    vin_vehiculo = fields.Char(string='VIN', store=True, required=True)
+    placa_vehiculo = fields.Char(string='Placa', store=True, required=True)
+    kilometraje_vehiculo = fields.Char(string='Kilometraje', store=True, required=True)
+    year_vehiculo = fields.Integer(
+        string='Año',
+        store=True,
+        required=True
+    )
+
+    @api.constrains('year_vehiculo')
+    def _check_year_format(self):
+        for record in self:
+            if record.year_vehiculo:
+                # Validación para 4 dígitos (1000-9999)
+                if record.year_vehiculo < 1000 or record.year_vehiculo > 9999:
+                    raise ValidationError("El año debe ser un número de exactamente 4 dígitos (ej: 2023)")
+                              
 
 class SecuenciaMes(models.Model):
     _inherit = 'sale.order'
