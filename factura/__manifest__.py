e39d7f5de035813c60e73c9fc1d16893f127d001
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-05-05 10:08:13 -0400

    Media Carta

---
+++ factura/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+{
+    'name': 'Factura v18',
+    'version': '1.0',
+    'depends': [
+        'base', 
+       
+    ],
+    'author': 'APPEX BOLIVIA SRL.',
+    'summary': 'Factura',
+    'data': [
+    'reports/formato_papel.xml',
+      'reports/boton_imprimir_pdf.xml',
+      'reports/cabecera_pdf.xml',
+      'reports/cuerpo_pdf.xml',
+    ],
+    'installable': True,
+}
