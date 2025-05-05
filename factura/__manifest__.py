71d256e0a6ddd88cf73d03fa26db8b5b27330eab
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-05-05 10:57:40 -0400

    Factura personalizada

--- factura/__manifest__.py
+++ factura/__manifest__.py
@@ -4,14 +4,14 @@
     'version': '1.0',
     'depends': [
         'base', 
-       
+       'l10n_bo_bolivian_invoice',
+       'account',
     ],
     'author': 'APPEX BOLIVIA SRL.',
     'summary': 'Factura',
     'data': [
     'reports/formato_papel.xml',
       'reports/boton_imprimir_pdf.xml',
-      'reports/cabecera_pdf.xml',
       'reports/cuerpo_pdf.xml',
     ],
     'installable': True,
