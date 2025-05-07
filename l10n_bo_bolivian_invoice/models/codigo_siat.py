b0578c7c2ba9a81ec41bf5c5378ccbc5ec5139df
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-05-07 17:21:40 -0400

    Ajuste

--- l10n_bo_bolivian_invoice/models/codigo_siat.py
+++ l10n_bo_bolivian_invoice/models/codigo_siat.py
@@ -7,5 +7,5 @@ class AccountMove(models.Model):
         self.ensure_one()  # Para evitar procesar múltiples movimientos a la vez
         if self.invoice_line_ids:
             first_product = self.invoice_line_ids[0].product_id
-            return first_product.siat_service_id.name if first_product.siat_service_id else ''
+            return first_product.siat_service_id.codigoActividad if first_product.siat_service_id else ''
         return 'No funciona'
